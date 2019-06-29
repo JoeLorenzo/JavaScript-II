ed5f80f13a7adabacb9cb99657d655fce4e4cbe7
JoeLorenzo <Joselmedrano@gmail.com>
2019-06-29 02:10:25 -0500

    Completed Callbacks part.  Still need to finish Closure.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,13 +2,13 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+
+  //Given this problem:
+
+  // function firstItem(arr, cb) {
+  //   // firstItem passes the first item of the given array to the callback function.
+  // }
 
   // Potential Solution:
 
@@ -17,34 +17,64 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
-*/
+
 
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(arrayLength) {
+  console.log(arrayLength);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[(arr.length-1)]);
 }
 
+last(items, function(lastElement){
+  console.log(lastElement);
+});
+
+
+let x = 4 ;
+let y = 6;
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
 
+sumNums(x, y, function(summedNumber){
+  console.log(summedNumber);
+})
+
+x = 3;
+y = 11;
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+return cb(x*y);
 }
 
+multiplyNums(x, y, function(product){
+  console.log(product);
+})
+let item = "yo-yo";
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+contains(item, items, function(containsItem){
+  console.log(containsItem);
+})
 
 /* STRETCH PROBLEM */
 
